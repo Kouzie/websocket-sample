e842a1e3f1bd50de49d48c4cb171fad9528fe3b2
kgy1996 <kgy1996@naver.com>
2020-12-22 15:09:26 +0900

    init stomp web socket project

--- src/main/java/com/example/websock/controller/WebPageController.java
+++ src/main/java/com/example/websock/controller/WebPageController.java
@@ -9,12 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebPageController {
 
-    @GetMapping("/sample")
-    public void websocket_sample(Model model) {
-        log.info("websocket_sample called...");
-        model.addAttribute("result", "SUCCESS");
-    }
-
     @GetMapping("/stomp")
     public void websocket_stomp(Model model) {
         log.info("websocket_stomp called...");
